491d7034c9cadd2003c01ce9f42eab5541fe2b2e
Dynamitos <dynamitos15@gmail.com>
2021-04-14 09:16:10 +0200

    FW-013 Make New Task naviable [SH,JH]

--- app/src/main/java/com/team06/focuswork/ui/tasks/NewTaskFragment.kt
+++ app/src/main/java/com/team06/focuswork/ui/tasks/NewTaskFragment.kt
@@ -1,7 +1,5 @@
 package com.team06.focuswork.ui.tasks
 
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 import android.os.Bundle
@@ -15,12 +13,12 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.Toast
-
-import com.team06.focuswork.ui.tasks.R
+import com.team06.focuswork.R
+import com.team06.focuswork.data.Task
 
 class NewTaskFragment : Fragment() {
 
-    private lateinit var loginViewModel: LoginViewModel
+    private lateinit var workingTask: Task
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -32,79 +30,16 @@ class NewTaskFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        loginViewModel = ViewModelProvider(this, LoginViewModelFactory())
-                .get(LoginViewModel::class.java)
 
-        val usernameEditText = view.findViewById<EditText>(R.id.username)
-        val passwordEditText = view.findViewById<EditText>(R.id.password)
-        val loginButton = view.findViewById<Button>(R.id.login)
-        val loadingProgressBar = view.findViewById<ProgressBar>(R.id.loading)
+        //val usernameEditText = view.findViewById<EditText>(R.id.taskName)
+        //val passwordEditText = view.findViewById<EditText>(R.id.taskDescription)
+        //val loginButton = view.findViewById<Button>(R.id.login)
+        //val loadingProgressBar = view.findViewById<ProgressBar>(R.id.loading)
 
-        loginViewModel.loginFormState.observe(this,
-                Observer { loginFormState ->
-                    if (loginFormState == null) {
-                        return@Observer
-                    }
-                    loginButton.isEnabled = loginFormState.isDataValid
-                    loginFormState.usernameError?.let {
-                        usernameEditText.error = getString(it)
-                    }
-                    loginFormState.passwordError?.let {
-                        passwordEditText.error = getString(it)
-                    }
-                })
-
-        loginViewModel.loginResult.observe(this,
-                Observer { loginResult ->
-                    loginResult ?: return@Observer
-                    loadingProgressBar.visibility = View.GONE
-                    loginResult.error?.let {
-                        showLoginFailed(it)
-                    }
-                    loginResult.success?.let {
-                        updateUiWithUser(it)
-                    }
-                })
-
-        val afterTextChangedListener = object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
-                // ignore
-            }
-
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                // ignore
-            }
-
-            override fun afterTextChanged(s: Editable) {
-                loginViewModel.loginDataChanged(
-                        usernameEditText.text.toString(),
-                        passwordEditText.text.toString()
-                )
-            }
-        }
-        usernameEditText.addTextChangedListener(afterTextChangedListener)
-        passwordEditText.addTextChangedListener(afterTextChangedListener)
-        passwordEditText.setOnEditorActionListener { _, actionId, _ ->
-            if (actionId == EditorInfo.IME_ACTION_DONE) {
-                loginViewModel.login(
-                        usernameEditText.text.toString(),
-                        passwordEditText.text.toString()
-                )
-            }
-            false
-        }
-
-        loginButton.setOnClickListener {
-            loadingProgressBar.visibility = View.VISIBLE
-            loginViewModel.login(
-                    usernameEditText.text.toString(),
-                    passwordEditText.text.toString()
-            )
-        }
     }
 
-    private fun updateUiWithUser(model: LoggedInUserView) {
-        val welcome = getString(R.string.welcome) + model.displayName
+    private fun updateUiWithUser(model: Task) {
+        val welcome = getString(R.string.welcome)
         // TODO : initiate successful logged in experience
         val appContext = context?.applicationContext ?: return
         Toast.makeText(appContext, welcome, Toast.LENGTH_LONG).show()
