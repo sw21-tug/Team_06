6b497ecc147e2dbfeb123d2249c72612247e8b42
Jonathan Resch <jonathan.resch@student.tugraz.at>
2021-04-16 19:10:46 +0200

    FW-012 Change menu icon of the OverViewFragment, add Testdata, change icon for adding new items [ED,RJ]

--- app/src/main/java/com/team06/focuswork/ui/overview/TaskAdapter.kt
+++ app/src/main/java/com/team06/focuswork/ui/overview/TaskAdapter.kt
@@ -8,12 +8,20 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.team06.focuswork.R
+import com.team06.focuswork.data.Task
+import java.util.*
 
 class TaskAdapter : RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
 
-    private val list = ('A').rangeTo('Z').toList()
+    private val list = mutableListOf<Task>()
 
     init {
+        list.add(Task("Erste Aufgabe", "Dies ist eine Aufgabenbeschr.", Calendar.getInstance(), Calendar.getInstance()))
+        list.add(Task("Zweite Aufgabe", "Dies ist weitere Aufgabenbeschr.", Calendar.getInstance(), Calendar.getInstance()))
+        list.add(Task("Dritte Aufgabe", "Dies ist noch eine Aufgabenbeschr.", Calendar.getInstance(), Calendar.getInstance()))
+        list.add(Task("Vierte Aufgabe", "Dies ist auch eine Aufgabenbeschr.", Calendar.getInstance(), Calendar.getInstance()))
+        list.add(Task("Fuenfte Aufgabe", "Dies ist eine tolle Aufgabenbeschr.", Calendar.getInstance(), Calendar.getInstance()))
+
         Log.d("TaskAdapter", list.toString())
     }
 
@@ -23,17 +31,17 @@ class TaskAdapter : RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
         val layout = LayoutInflater
-            .from(parent.context)
-            .inflate(R.layout.task_item_view, parent, false)
+                .from(parent.context)
+                .inflate(R.layout.task_item_view, parent, false)
         return TaskViewHolder(layout)
     }
 
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
         val item = list[position]
-        holder.taskItem.text = item.toString()
-        Log.d("TaskAdapter", item.toString())
+        holder.taskItem.text = item.taskName
+        Log.d("TaskAdapter", item.taskName)
         holder.taskItem.setOnClickListener {
-            Toast.makeText(holder.view.context, item.toString(), Toast.LENGTH_LONG).show()
+            Toast.makeText(holder.view.context, item.taskName + ": " + item.taskDescription, Toast.LENGTH_LONG).show()
             //TODO: open new task fragment here
         }
     }
