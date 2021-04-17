d15572c435b299e4c09bd122cb42437fd644e6f8
David Egger <david.egga@gmx.at>
2021-04-17 17:41:53 +0200

    FW-012 Beautify FragmentOverview[ED,RJ]

--- app/src/main/java/com/team06/focuswork/ui/overview/OverviewFragment.kt
+++ app/src/main/java/com/team06/focuswork/ui/overview/OverviewFragment.kt
@@ -1,17 +1,13 @@
 package com.team06.focuswork.ui.overview
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.team06.focuswork.R
 import com.team06.focuswork.databinding.FragmentOverviewBinding
 
 class OverviewFragment : Fragment() {
--- app/src/main/java/com/team06/focuswork/ui/overview/TaskAdapter.kt
+++ app/src/main/java/com/team06/focuswork/ui/overview/TaskAdapter.kt
@@ -1,5 +1,7 @@
 package com.team06.focuswork.ui.overview
 
+import android.graphics.Color
+import android.text.format.DateFormat
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -7,41 +9,94 @@ import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.card.MaterialCardView
 import com.team06.focuswork.R
 import com.team06.focuswork.data.Task
 import java.util.*
+
 
 class TaskAdapter : RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
 
     private val list = mutableListOf<Task>()
 
     init {
-        list.add(Task("Erste Aufgabe", "Dies ist eine Aufgabenbeschr.", Calendar.getInstance(), Calendar.getInstance()))
-        list.add(Task("Zweite Aufgabe", "Dies ist weitere Aufgabenbeschr.", Calendar.getInstance(), Calendar.getInstance()))
-        list.add(Task("Dritte Aufgabe", "Dies ist noch eine Aufgabenbeschr.", Calendar.getInstance(), Calendar.getInstance()))
-        list.add(Task("Vierte Aufgabe", "Dies ist auch eine Aufgabenbeschr.", Calendar.getInstance(), Calendar.getInstance()))
-        list.add(Task("Fuenfte Aufgabe", "Dies ist eine tolle Aufgabenbeschr.", Calendar.getInstance(), Calendar.getInstance()))
+        list.add(
+            Task(
+                "Erste Aufgabe",
+                "Dies ist eine Aufgabenbeschr.",
+                Calendar.getInstance(),
+                Calendar.getInstance()
+            )
+        )
+        list.add(
+            Task(
+                "Zweite Aufgabe",
+                "Dies ist weitere Aufgabenbeschr.",
+                Calendar.getInstance(),
+                Calendar.getInstance()
+            )
+        )
+        list.add(
+            Task(
+                "Dritte Aufgabe",
+                "Dies ist noch eine Aufgabenbeschr.",
+                Calendar.getInstance(),
+                Calendar.getInstance()
+            )
+        )
+        list.add(
+            Task(
+                "Vierte Aufgabe",
+                "Dies ist auch eine Aufgabenbeschr.",
+                Calendar.getInstance(),
+                Calendar.getInstance()
+            )
+        )
+        list.add(
+            Task(
+                "Fuenfte Aufgabe",
+                "Dies ist eine tolle Aufgabenbeschr.",
+                Calendar.getInstance(),
+                Calendar.getInstance()
+            )
+        )
 
         Log.d("TaskAdapter", list.toString())
     }
 
     class TaskViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-        val taskItem = view.findViewById<TextView>(R.id.task_item)
+        val taskItem = view.findViewById<MaterialCardView>(R.id.task_item)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
         val layout = LayoutInflater
-                .from(parent.context)
-                .inflate(R.layout.task_item_view, parent, false)
+            .from(parent.context)
+            .inflate(R.layout.task_item_view, parent, false)
         return TaskViewHolder(layout)
     }
 
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
         val item = list[position]
-        holder.taskItem.text = item.taskName
+        val dateFormat: java.text.DateFormat? = DateFormat.getTimeFormat(holder.view.context)
+
+        holder.taskItem.findViewById<TextView>(R.id.task_item_title).text = item.taskName
+        val startTimeText : TextView = holder.taskItem.findViewById(R.id.task_item_start_time)
+        startTimeText.text = dateFormat?.format(item.startTime.time)
+        val endTimeTextView : TextView = holder.taskItem.findViewById(R.id.task_item_end_time)
+        endTimeTextView.text = dateFormat?.format(item.duration.time)
+
+        if (position % 2 == 0)
+            holder.taskItem.setBackgroundColor(Color.rgb(215, 222, 252))
+        else
+            holder.taskItem.setBackgroundColor(Color.rgb(168, 178, 225))
+
         Log.d("TaskAdapter", item.taskName)
         holder.taskItem.setOnClickListener {
-            Toast.makeText(holder.view.context, item.taskName + ": " + item.taskDescription, Toast.LENGTH_LONG).show()
+            Toast.makeText(
+                holder.view.context,
+                item.taskName + ": " + item.taskDescription,
+                Toast.LENGTH_LONG
+            ).show()
             //TODO: open new task fragment here
         }
     }
