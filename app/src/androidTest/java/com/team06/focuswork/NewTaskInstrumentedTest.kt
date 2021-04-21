5ab1753bf242b46ec5d1405ff6baa32e526e3918
Dynamitos <dynamitos15@gmail.com>
2021-04-21 11:37:04 +0200

    FW-013 Creating second UI test [HS,GF]

--- app/src/androidTest/java/com/team06/focuswork/NewTaskInstrumentedTest.kt
+++ app/src/androidTest/java/com/team06/focuswork/NewTaskInstrumentedTest.kt
@@ -1,21 +1,23 @@
 package com.team06.focuswork
 
-import androidx.fragment.app.testing.launchFragmentInContainer
+import android.widget.DatePicker
+import android.widget.TimePicker
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.DrawerActions
 import androidx.test.espresso.contrib.NavigationViewActions
-import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.contrib.PickerActions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.team06.focuswork.ui.tasks.NewTaskFragment
 import org.hamcrest.CoreMatchers.not
+import org.hamcrest.Matchers
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+
 
 @RunWith(AndroidJUnit4::class)
 class NewTaskInstrumentedTest {
@@ -32,40 +34,57 @@ class NewTaskInstrumentedTest {
                 .perform(NavigationViewActions.navigateTo(R.id.nav_new_task))
 
         // Wait short amount of time to ensure everything has loaded
-        Thread.sleep(200)
+        Thread.sleep(400)
         onView(withId(R.id.containerCreateTask))
             .check(matches(isDisplayed()))
     }
-
+    private fun setupTaskStrings(taskName: String, taskDescription: String) {
+        onView(withId(R.id.taskName))
+            .perform(typeText(taskName))
+        onView(withId(R.id.taskDescription))
+            .perform(typeText(taskDescription))
+        onView(isRoot())
+            .perform(closeSoftKeyboard())
+    }
+    private fun setStartDateValues(year: Int, month: Int, day: Int) {
+        onView(withId(R.id.taskStartDate))
+            .perform(click())
+        onView(withClassName(Matchers.equalTo(DatePicker::class.java.name)))
+            .perform(PickerActions.setDate(year, month, day))
+        onView(withId(android.R.id.button1)).perform(click())
+    }
+    private fun setStartTimeValues(hour: Int, minute: Int) {
+        onView(withId(R.id.taskStartTime))
+            .perform(click())
+        onView(withClassName(Matchers.equalTo(TimePicker::class.java.name)))
+            .perform(PickerActions.setTime(hour, minute))
+        onView(withId(android.R.id.button1)).perform(click())
+    }
+    private fun setEndDateValues(year: Int, month: Int, day: Int) {
+        onView(withId(R.id.taskEndDate))
+            .perform(click())
+        onView(withClassName(Matchers.equalTo(DatePicker::class.java.name)))
+            .perform(PickerActions.setDate(year, month, day))
+        onView(withId(android.R.id.button1)).perform(click())
+    }
+    private fun setEndTimeValues(hour: Int, minute: Int) {
+        onView(withId(R.id.taskEndTime))
+            .perform(click())
+        onView(withClassName(Matchers.equalTo(TimePicker::class.java.name)))
+            .perform(PickerActions.setTime(hour, minute))
+        onView(withId(android.R.id.button1)).perform(click())
+    }
     @Test
     fun createSimpleTask() {
         // At first, Task Create Button should not be enabled
         onView(withId(R.id.taskCreate))
                 .check(matches(not(isEnabled())))
 
-        // Set name and description
-        onView(withId(R.id.taskName))
-                .perform(typeText("TaskName"))
-        onView(withId(R.id.taskDescription))
-                .perform(typeText("Task Description"))
-        onView(isRoot())
-            .perform(closeSoftKeyboard())
-
-        // Set Start and End
-        /*
-        //TODO check dialog that opens
-        onView(withId(R.id.taskStartDate))
-           .perform(click())
-        onView(withId(R.id.date))
-        onView(withId() )
-                //PickerActions.setDate(2021, 6, 1))
-        onView(withId(R.id.taskStartTime))
-                .perform(PickerActions.setTime(8, 30))
-        onView(withId(R.id.taskEndDate))
-                .perform(PickerActions.setDate(2021, 6, 1))
-        onView(withId(R.id.taskEndTime))
-                .perform(PickerActions.setTime(13, 45))
-        */
+        setupTaskStrings("Task Name", "Task Description");
+        setStartDateValues(2022, 10, 22)
+        setStartTimeValues(10, 0)
+        setEndDateValues(2022, 10, 22)
+        setEndTimeValues(11, 0)
 
         // Task Create Button should now be enabled
         onView(withId(R.id.taskCreate))
@@ -74,6 +93,22 @@ class NewTaskInstrumentedTest {
 
         // After click, overview should be shown again
         onView(withId(R.id.frame_layout_overview))
-            .check(matches(isDisplayed()))
+                .check(matches(isDisplayed()))
+    }
+    @Test
+    fun namelessTask() {
+        // At first, Task Create Button should not be enabled
+        onView(withId(R.id.taskCreate))
+            .check(matches(not(isEnabled())))
+
+        setupTaskStrings("", "Task Description");
+        setStartDateValues(2022, 10, 22)
+        setStartTimeValues(10, 0)
+        setEndDateValues(2022, 10, 22)
+        setEndTimeValues(9, 0)
+
+        // Task Create Button should still be disabled
+        onView(withId(R.id.taskCreate))
+            .check(matches(not(isEnabled())))
     }
 }
