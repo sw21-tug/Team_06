fa7443fabe25aae9e6aadeabad94fb0229e913f2
Hagen <mercypl0x@gmx.at>
2021-05-05 20:51:28 +0200

    FW-036 improved TaskDetailsFragment [LH,ED]

--- app/src/main/java/com/team06/focuswork/ui/taskdetails/TaskdetailsFragment.kt
+++ app/src/main/java/com/team06/focuswork/ui/taskdetails/TaskdetailsFragment.kt
@@ -1,6 +1,8 @@
 package com.team06.focuswork.ui.taskdetails
 
 import android.os.Bundle
+import android.text.Layout
+import android.text.format.DateFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,40 +11,46 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import com.team06.focuswork.R
+import com.team06.focuswork.databinding.FragmentTaskdetailsBinding
 import com.team06.focuswork.model.TasksViewModel
 
 class TaskdetailsFragment : Fragment() {
 
     private val tasksViewModel: TasksViewModel by activityViewModels()
+    private lateinit var binding: FragmentTaskdetailsBinding
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentTaskdetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
-        val root = inflater.inflate(R.layout.fragment_taskdetails, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        val titleView: TextView = root.findViewById(R.id.title_of_taskdetails)
-        tasksViewModel.title.observe(viewLifecycleOwner, Observer {
-            titleView.text = it
+        val titleView: TextView = binding.titleTaskdetails
+        val descriptionView: TextView = binding.descriptionTaskdetails
+        val starttimeView: TextView = binding.starttimeTaskdetails
+        val endtimeView: TextView = binding.endtimeTaskdetails
+        val startdateView: TextView = binding.startdateTaskdetails
+        val enddateView: TextView = binding.enddateTaskdetails
+
+        val timeFormat: java.text.DateFormat = DateFormat.getTimeFormat(context)
+        val dateFormat: java.text.DateFormat = DateFormat.getDateFormat(context)
+
+
+        tasksViewModel.currentTask.observe(viewLifecycleOwner, Observer {
+            titleView.text = it.taskName
+            descriptionView.text = it.taskDescription
+            starttimeView.text = timeFormat.format(it.startTime.time)
+            endtimeView.text = timeFormat.format(it.endTime.time)
+            startdateView.text = dateFormat.format(it.startTime.time)
+            enddateView.text = dateFormat.format(it.endTime.time)
+
         })
 
-        val descriptionView: TextView = root.findViewById(R.id.description_of_taskdetails)
-        tasksViewModel.description.observe(viewLifecycleOwner, Observer {
-            descriptionView.text = it
-        })
-
-        val starttimeView: TextView = root.findViewById(R.id.starttime_of_taskdetails)
-        tasksViewModel.startTime.observe(viewLifecycleOwner, Observer {
-            starttimeView.text = it
-        })
-
-        val durationView: TextView = root.findViewById(R.id.duration_of_taskdetails)
-        tasksViewModel.endTime.observe(viewLifecycleOwner, Observer {
-            durationView.text = it
-        })
-
-        return root
     }
 }
