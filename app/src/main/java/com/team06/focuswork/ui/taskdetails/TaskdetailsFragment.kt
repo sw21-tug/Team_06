2db94736affb3b5236dd4bef75dc049762c9c9bf
Jonathan Resch <jonathan.resch@student.tugraz.at>
2021-05-19 10:10:35 +0200

    FW-033 Update task detail fragment for timer: add functionality [LH,RJ]

--- app/src/main/java/com/team06/focuswork/ui/taskdetails/TaskdetailsFragment.kt
+++ app/src/main/java/com/team06/focuswork/ui/taskdetails/TaskdetailsFragment.kt
@@ -1,21 +1,27 @@
 package com.team06.focuswork.ui.taskdetails
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.text.format.DateFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
+import com.team06.focuswork.R
+import com.team06.focuswork.data.Task
 import com.team06.focuswork.databinding.FragmentTaskdetailsBinding
 import com.team06.focuswork.model.TasksViewModel
+import java.util.*
 
 class TaskdetailsFragment : Fragment() {
 
     private val tasksViewModel: TasksViewModel by activityViewModels()
     private lateinit var binding: FragmentTaskdetailsBinding
+    private var taskTimer: CountDownTimer? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,6 +45,8 @@ class TaskdetailsFragment : Fragment() {
         val timeFormat: java.text.DateFormat = DateFormat.getTimeFormat(context)
         val dateFormat: java.text.DateFormat = DateFormat.getDateFormat(context)
 
+        binding.taskTimer.text = String.format(getString(R.string.timer_countdown), 0, 0)
+
         tasksViewModel.currentTask.observe(viewLifecycleOwner, Observer {
             titleView.text = it.taskName
             descriptionView.text = it.taskDescription
@@ -47,7 +55,24 @@ class TaskdetailsFragment : Fragment() {
             startdateView.text = dateFormat.format(it.startTime.time)
             enddateView.text = dateFormat.format(it.endTime.time)
 
+            onStartTimer(it)
         })
 
+    }
+
+    fun onStartTimer(task: Task){
+        val duration = task.endTime.timeInMillis - Calendar.getInstance().timeInMillis
+
+        taskTimer = object : CountDownTimer(duration, 10 * 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                val hours = millisUntilFinished / (60 * 60 * 1000)
+                val minutes = millisUntilFinished / (60 * 1000) % (60)
+                binding.taskTimer.text =String.format(getString(R.string.timer_countdown), hours, minutes)
+            }
+
+            override fun onFinish() {
+                Toast.makeText(context?.applicationContext, "Task finished", Toast.LENGTH_LONG).show()
+            }
+        }.start()
     }
 }
