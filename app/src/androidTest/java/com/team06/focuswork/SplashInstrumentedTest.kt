37aceb95d5fe0931ca4e546e35e3cc3123c9fa2f
Jan Hoffmann <hoffmann@student.tugraz.at>
2021-06-09 10:06:16 +0200

    FW-046 Fix SplashScreenTest with AutoLogin [HJ, LS]

--- app/src/androidTest/java/com/team06/focuswork/SplashInstrumentedTest.kt
+++ app/src/androidTest/java/com/team06/focuswork/SplashInstrumentedTest.kt
@@ -10,6 +10,7 @@ import com.team06.focuswork.ui.splashscreen.SplashScreen
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.lang.Exception
 
 
 @RunWith(AndroidJUnit4::class)
@@ -23,6 +24,12 @@ class SplashInstrumentedTest {
     fun splashScreenTest() {
         onView(withId(R.id.logoImage)).check(matches(isDisplayed()))
         Thread.sleep(3000)
-        onView(withId(R.id.login)).check(matches(isDisplayed()))
+        try {
+            //Passes if AutoLogin Fails
+            onView(withId(R.id.login)).check(matches(isDisplayed()))
+        } catch (ex: Exception){
+            //Fails if AutoLogin happens, so we need to check the main View instead
+            onView(withId(R.id.recycler_view)).check(matches(isDisplayed()))
+        }
     }
 }
