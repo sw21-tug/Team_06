51582d9cb75dab3352f8b28d1de15f035e1feff9
Jan Hoffmann <hoffmann@student.tugraz.at>
2021-06-09 08:20:18 +0200

    FW-022 Removed unneeded Imports [HJ, LH]

--- app/src/main/java/com/team06/focuswork/ui/overview/OverviewFragment.kt
+++ app/src/main/java/com/team06/focuswork/ui/overview/OverviewFragment.kt
@@ -1,14 +1,12 @@
 package com.team06.focuswork.ui.overview
 
 import android.content.SharedPreferences
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import android.widget.Toolbar
 import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
