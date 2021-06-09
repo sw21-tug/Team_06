70a461b7851ff2aa736411450d42a845103ea9b0
Mercypl0x <mercypl0x@gmx.at>
2021-06-09 08:17:08 +0200

    FW-022 AccentColour Tests now Work [LH, HJ]

--- app/src/androidTest/java/com/team06/focuswork/ThemeInstrumentedTest.kt
+++ app/src/androidTest/java/com/team06/focuswork/ThemeInstrumentedTest.kt
@@ -18,6 +18,7 @@ import com.team06.focuswork.espressoUtil.MockUtil
 import com.team06.focuswork.espressoUtil.NavigationUtil
 import com.team06.focuswork.espressoUtil.PrepareValuesUtil
 import com.team06.focuswork.espressoUtil.ThemeUtil
+import com.team06.focuswork.espressoUtil.ThemeUtil.withAccentColor
 import com.team06.focuswork.espressoUtil.ThemeUtil.withBackgroundColor
 import com.team06.focuswork.model.LoggedInUser
 import org.hamcrest.Description
@@ -37,7 +38,6 @@ class ThemeInstrumentedTest {
         ActivityScenarioRule(MainActivity::class.java)
 
     private val navigator = NavigationUtil()
-    private val valueSetter = PrepareValuesUtil()
     private val user = LoggedInUser("SfuvPQ8Uf2wistKapXBQ")
     private val backgrounds = InstrumentationRegistry.getInstrumentation()
         .targetContext.resources.getStringArray(R.array.color_background_entries)
@@ -46,6 +46,8 @@ class ThemeInstrumentedTest {
 
     private val backgroundColourDark = 0xFF232323.toInt()
     private val backgroundColourLight = 0xFFE9E9E9.toInt()
+    private val accentColourRed = 0xFFED413E.toInt()
+    private val accentColourBlue = 0xFF4E84F5.toInt()
 
     @Before
     fun init() {
@@ -55,7 +57,6 @@ class ThemeInstrumentedTest {
     @Test
     fun testDarkBackground() {
         navigator.chooseSetting(R.string.colorBackground_title, backgrounds[1])
-        //navigator.chooseSetting(R.string.colorAccent_title, accents[1])
         navigator.navigateToOverview()
         Thread.sleep(400)
 
@@ -65,10 +66,27 @@ class ThemeInstrumentedTest {
     @Test
     fun testLightBackground() {
         navigator.chooseSetting(R.string.colorBackground_title, backgrounds[0])
-        //navigator.chooseSetting(R.string.colorAccent_title, accents[1])
         navigator.navigateToOverview()
         Thread.sleep(400)
 
         onView(withId(R.id.recycler_view)).check(matches(withBackgroundColor(backgroundColourLight)))
+    }
+
+    @Test
+    fun testRedAccent() {
+        navigator.chooseSetting(R.string.colorAccent_title, accents[0])
+        navigator.navigateToOverview()
+        Thread.sleep(400)
+
+        onView(withId(R.id.toolbar)).check(matches(withAccentColor(accentColourRed)))
+    }
+
+    @Test
+    fun testBlueAccent() {
+        navigator.chooseSetting(R.string.colorAccent_title, accents[1])
+        navigator.navigateToOverview()
+        Thread.sleep(400)
+
+        onView(withId(R.id.toolbar)).check(matches(withAccentColor(accentColourBlue)))
     }
 }
--- app/src/androidTest/java/com/team06/focuswork/espressoUtil/ThemeUtil.kt
+++ app/src/androidTest/java/com/team06/focuswork/espressoUtil/ThemeUtil.kt
@@ -13,34 +13,11 @@ import org.hamcrest.Matcher
 import java.lang.reflect.Method
 
 object ThemeUtil {
-    /* Nicely, this is unable to read any attributes
-    val a = context.theme.obtainStyledAttributes(
-        R.style.Theme_FocusWork_Dark,
-        intArrayOf(R.attr.backgroundColor))
-    val t = TypedValue()
-    a.getValue(0, t)
-    a
-
-     */
-    fun getThemeBackground(context: Context, theme: Int): Int {
-        val arr = context.theme.obtainStyledAttributes(
-                theme,
-                IntArray(1) { R.attr.background })
-        val ret = arr.getColor(0, 0)
-        arr.recycle()
-        return ret
-    }
-
     fun withBackgroundColor(color: Int): Matcher<View?>? {
         Checks.checkNotNull(color)
         return object : BoundedMatcher<View?, View>(View::class.java) {
             var actual = 0
             override fun matchesSafely(view: View): Boolean {
-                //val localBinding = dynamicBinding as FragmentDayBinding
-                //val root = view.rootView
-                //val draw = root.background as ColorDrawable
-                //String.format("#%06X", (0xFFFFFF and draw.color))
-
                 actual = (view.rootView.background as ColorDrawable).color
                 Log.d("ThemeTest", "Color is: $color and actual is: $actual")
                 return color == actual
@@ -48,6 +25,22 @@ object ThemeUtil {
 
             override fun describeTo(description: Description) {
                 description.appendText("with Background color: Got "
+                    + Integer.toHexString(actual) + " but expected " + Integer.toHexString(color))
+            }
+        }
+    }
+
+    fun withAccentColor(color: Int): Matcher<View?>? {
+        Checks.checkNotNull(color)
+        return object : BoundedMatcher<View?, View>(View::class.java) {
+            var actual = 0
+            override fun matchesSafely(view: View): Boolean {
+                actual = (view.background as ColorDrawable).color
+                return color == actual
+            }
+
+            override fun describeTo(description: Description) {
+                description.appendText("with Accent color: Got "
                     + Integer.toHexString(actual) + " but expected " + Integer.toHexString(color))
             }
         }
